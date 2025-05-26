dda97e0e594995b1cf407d239dac81bee26138d0
abishop0894 <abishop0894@gmail.com>
2025-05-26 15:35:05 -0400

    new

---
+++ app/modules/components/cta/MarqueeComp.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Marquee } from "./layout/Maquee"
+
+
+export const MarqueeComp = ({title}: {title: string}) => {
+    return (
+<div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
+      <Marquee className="[--duration:20s]">
+      <div className="flex items-center justify-center">
+        <h1 className="text-[89px] md:m-[6vw] opacity-50 font-bold">{title}</h1>
+      </div>
+      </Marquee>
+    </div>
+    )
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "jrdisplays.s3.us-east-1.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
