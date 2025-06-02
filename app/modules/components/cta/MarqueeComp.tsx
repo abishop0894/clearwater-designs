ef9b4b033b720ac8f514241c598074344e6ec1bf
abishop0894 <abishop0894@gmail.com>
2025-06-01 21:34:22 -0400

    new

--- app/modules/components/cta/MarqueeComp.tsx
+++ app/modules/components/cta/MarqueeComp.tsx
@@ -6,7 +6,7 @@ import { Marquee } from "./layout/Maquee"
 export const MarqueeComp = ({title}: {title: string}) => {
     return (
 <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-      <Marquee className="[--duration:50s]">
+      <Marquee className="[--duration:30s]">
       <div className="flex items-center justify-center">
         <h1 className="text-[89px] md:m-[6vw] opacity-50 font-bold">{title}</h1>
       </div>
