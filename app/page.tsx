b07cff8c2b0e8c83c74908d3889e9fb25030d1d1
abishop0894 <abishop0894@gmail.com>
2025-06-10 15:09:50 -0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-
-import { HomeHero } from "./modules/components/hero/HomeHero";
 import Page from "./modules/layout/Page";
 import SectionHeading from "./modules/components/cta/SectionHeading";
 import SectionNumber from "./modules/components/cta/layout/SectionNumber";
@@ -7,11 +5,12 @@ import AnimatedParagraph from "./modules/components/cta/layout/AnimatedParagraph
 import Carousel from "./modules/components/cta/Carousel";
 import { portfolioCards } from "@/lib/constants";
 import FixedSectionHeadingWithCards from "./modules/components/sections/FixedSectionHeadingWithCards";
+import HeroTwo from "./modules/components/hero/HeroTwo";
 
 export default function Home() {
   return (
   <Page>
-    <HomeHero />  
+    <HeroTwo/>
     <SectionHeading title="Our Services" backgroundImg= "https://jrdisplays.s3.us-east-1.amazonaws.com/portfolio/interior/IMG-20240410-WA0001.jpg" />
     <SectionNumber text="IV" />
     <AnimatedParagraph 
